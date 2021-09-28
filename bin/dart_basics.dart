f0110b1d4905c42f80fc411589702e29aa0f8246
Heinsed <vkinev6@gmail.com>
2021-09-28 15:01:28 +0300

    Changed methods to class and Binary Calculator is done.

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,5 +1,11 @@
 import 'package:dart_basics/delimeters_calculator.dart' as dart_basics;
 
 void main() {
-  print('Multiplayers is: ${dart_basics.mult(100000)}');
+  var nums = dart_basics.Calc(100, 23);
+  nums.nod();
+  nums.nok();
+  nums.mult();
+
+  var bin = dart_basics.BinCode(11010010);
+  bin.decimal();
 }
--- lib/delimeters_calculator.dart
+++ lib/delimeters_calculator.dart
@@ -1,34 +1,89 @@
-int nod(int a, int b){
-   late int result;
+
+
+import 'dart:math';
+
+class Calc{
+   int a;
+   int b;
+
+  Calc(this.a, this.b);
+
+  void nod(){
+    var a = this.a;
+     var b = this.b;
+    late int result;
    for(int i=1; i<=a || i<=b; i++){
     if(a%i==0 && b%i == 0){
       result = i;
     }
   }
-  return result;
-}
-
-num nok(int a, int b){
-  late num result;
-  result = a*b/nod(a, b);
-  return result;
-}
-
-List<String> mult(num a){
-  late List<String> result = [];
-  int div = 2;
-
-  while (a > 1)
-  {
-    while (a % div == 0)
-    {
-     result.add(div.toString());
-     a = a / div;
-    }
-    div++;
+  print("NOD is: " + result.toString());
   }
 
-return result;
+  void nok(){
+    late num result;
+    late num nod;
+    int a = this.a;
+    int b = this.b;
+    for(int i=1; i<=a || i<=b; i++){
+      if(a%i==0 && b%i == 0){
+         nod = i;
+      }
+    }
+    result = a*b/nod;
+    print("NOK is: " + result.toString());
+  }
 
+  void mult(){
+    int div = 2;
+    num a = this.a;
+    num b = this.b;
+    print("Multipliers is:");
+    print("A: ");
+    while (a > 1){
+      while (a % div == 0){
+        print(div);
+        a = a / div;
+      }
+    div++;
+    }
+    print('');
+    print("B: ");
+    div = 2;
+    while (b > 1){
+      while (b % div == 0){
+        print(div);
+        b = b / div;
+      }
+    div++;
+    }
+  }
 }
+
+
+class BinCode{
+  late int number;
+  BinCode(this.number);
+
+  void bin(){
+    int number = this.number;
+    print("Binary of $number is: " + number.toRadixString(2));
+  }
+  void decimal(){
+    int number = this.number;
+    int numLength = number.toString().length;
+    num result = 0;
+
+    for(int i=0; i<numLength; i++){
+        if(number.toString()[i] == '1'){
+          num value = pow(2, numLength-1-i);
+          result += value;
+        }
+    }
+    print("Decimal value of $number is: " + result.toString());
+  }
+}
+
+
+
 
